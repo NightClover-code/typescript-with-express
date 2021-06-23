0fd84358a8c00fe5de09d50076b48dd273413751
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-23 14:55:21 +0100

    adding basic login form

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 //importing dependencies
 import express from 'express';
 import { router } from './routes/login';
+import bodyParser from 'body-parser';
 
 //init server
 const app = express();
 
 //middlewares
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(router);
 
 //listening
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,9 +1,30 @@
-import { Router } from 'express';
+//importing dependencies
+import { Router, Response, Request } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Hi there');
+//get route
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="email" type="email" />
+        </div>
+        <div>
+            <label>Password</label>
+            <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+    </form>
+  `);
+});
+
+//post route
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  res.send(email + password);
 });
 
 export { router };
