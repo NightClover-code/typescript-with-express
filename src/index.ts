d91b03c1e53605408557c462a211e99e2f60defb
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-23 14:44:05 +0100

    adding a server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+//importing dependencies
+import express, { Request, Response } from 'express';
+
+//init server
+const app = express();
+
+//routes
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+        <div>
+          <h1>Hi there!</h1>
+        </div>
+    `);
+});
+
+//listening
+const PORT = 4000;
+app.listen(PORT, () =>
+  console.log(`listening on port ${PORT}, visit http://localhost:${PORT}`)
+);
