c17969cac3e9c147ebe45cdd585d57c835e8ac2f
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-08-23 17:04:05 +0100

    adding a use decorator to use middlewares

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -18,9 +18,12 @@ export function controller(routePrefix: string) {
         target.prototype,
         key
       );
+      const middlewares =
+        Reflect.getMetadata(MetadataKeys.MIDDLEWARE, target.prototype, key) ||
+        [];
 
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       }
     }
   };
